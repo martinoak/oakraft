8b063b5f4d260c6b2dcd55da343ae7ba47e37ff2
Martin <martin.dub@dek-cz.com>
2025-06-16 20:04:27 +0200

    Vite bug

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    server: {
+        strictPort: true,
+        cors: true
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
