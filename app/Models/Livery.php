498844bf1cf721f42d0f123a6b6ae04f84fbdef2
Martin Dub <martin.dub@dek-cz.com>
2025-06-19 09:15:28 +0200

    Useless functions

--- app/Models/Livery.php
+++ app/Models/Livery.php
@@ -29,24 +29,4 @@ class Livery extends Model
         'discount_jpg' => 'decimal:2',
         'discount_png' => 'decimal:2',
     ];
-
-    public function scopeFeatured($query)
-    {
-        return $query->where('featured', true);
-    }
-
-    public function scopeForSale($query)
-    {
-        return $query->where('for_sale', true);
-    }
-
-    public function scopeSearch($query, $search)
-    {
-        return $query->where(function ($q) use ($search) {
-            $q->where('name', 'like', "%{$search}%")
-              ->orWhere('description', 'like', "%{$search}%")
-              ->orWhere('category', 'like', "%{$search}%")
-              ->orWhere('tags', 'like', "%{$search}%");
-        });
-    }
 }
